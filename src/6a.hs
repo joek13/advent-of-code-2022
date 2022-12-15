d17e06e8ebdc6b62f3f01b17f12eb0da8d914f3a
Joe Kerrigan <joek1301@gmail.com>
2022-12-15 18:15:20 -0500

    day 6 solutions

---
+++ src/6a.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Data.List (tails, find)
+import Data.Set (Set, empty, member, insert)
+import Data.Maybe (fromJust)
+
+uniq :: Ord a => [a] -> Bool
+uniq = uniq' empty 
+
+uniq' :: Ord a => Set a -> [a] -> Bool
+uniq' seen (x:xs) | x `member` seen = False
+                  | otherwise = uniq' (insert x seen) xs
+uniq' _ [] = True
+
+main :: IO ()
+main = do
+    input <- getLine
+    let seqs = map (take 4) $ tails input
+    let pairs  = zip [0..] seqs :: [(Int, [Char])]
+    let marker = fst $ fromJust $ find (uniq.snd) pairs
+    print $ 4 + marker
